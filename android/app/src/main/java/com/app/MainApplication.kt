d43f731d9ce247010ffacf3c45c151c63d86f9c1
yasi <yasi@example.com>
2026-02-14 00:13:31 +0330

    build(android): add android native configuration and gradle scripts

--- android/app/src/main/java/com/app/MainApplication.kt
+++ android/app/src/main/java/com/app/MainApplication.kt
@@ -6,17 +6,16 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+// اضافه کردن این import برای اطمینان
+import com.facebook.react.ReactPackage 
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactHost: ReactHost by lazy {
     getDefaultReactHost(
       context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
+      // اصلاح اصلی اینجاست: اضافه کردن .packages
+      packageList = PackageList(this).packages 
     )
   }
 
